46a20c3a17f4377326f02078155bc02240d4eb03
Magnus Gärtner <magnusgaertner@gmail.com>
2018-07-07 14:00:03 +0200

    some script adjustments, could be needed for the previous comments

--- stomp_setup.sh
+++ stomp_setup.sh
@@ -1,10 +1,32 @@
 #! /bin/bash
-# This script safely launches ros nodes with buffer time to allow param server population
-x-terminal-emulator -e roslaunch kuka_arm target_description.launch &
-sleep 3 &&
-x-terminal-emulator -e roslaunch kuka_arm cafe.launch &
-sleep 3 &&
-x-terminal-emulator -e roslaunch kuka_arm spawn_target.launch &
-sleep 5 &&
-#x-terminal-emulator -e roslaunch kuka_arm inverse_kinematics.launch
-roslaunch kuka_arm inverse_kinematics.launch
+gnome-terminal --tab -e "/bin/bash -c \
+
+'roscore;\
+exec /bin/bash -i'" --tab -e "/bin/bash -c \
+
+'sleep 2;\
+rosrun rqt_console rqt_console;\
+exec /bin/bash -i'" --tab -e "/bin/bash -c \
+
+'sleep 1;\
+roslaunch kuka_arm target_description.launch;\
+exec /bin/bash -i'" --tab -e "/bin/bash -c \
+
+'sleep 3;\
+roslaunch kuka_arm cafe.launch;\
+exec /bin/bash -i'" --tab -e "/bin/bash -c \
+
+'sleep 3;\
+roslaunch kuka_arm spawn_target.launch;\
+exec /bin/bash -i'" --tab -e "/bin/bash -c \
+
+'sleep 5;\
+roslaunch kuka_arm inverse_kinematics.launch;\
+exec /bin/bash -i'" --tab -e "/bin/bash -c \
+
+'roslaunch kuka_arm self_filter.launch;\
+exec /bin/bash -i'" --tab -e "/bin/bash -c \
+
+'sleep 5;\
+roslaunch voxblox_ros kr210.launch;\
+exec /bin/bash -i'"
