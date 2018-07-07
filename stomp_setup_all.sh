cb62ef5b3b25e1696c89d687a525805adf56d8ac
Magnus Gärtner <magnusgaertner@gmail.com>
2018-07-07 15:22:33 +0200

    changed script stomp_setup to start all but gazebo
    stomp_setup_all is backup and still starts everything

--- stomp_setup_all.sh
+++ stomp_setup_all.sh
@@ -1,9 +1,23 @@
 #! /bin/bash
 gnome-terminal --tab -e "/bin/bash -c \
 
+'roscore;\
+exec /bin/bash -i'" --tab -e "/bin/bash -c \
 
 'sleep 2;\
 rosrun rqt_console rqt_console;\
+exec /bin/bash -i'" --tab -e "/bin/bash -c \
+
+'sleep 1;\
+roslaunch kuka_arm target_description.launch;\
+exec /bin/bash -i'" --tab -e "/bin/bash -c \
+
+'sleep 3;\
+roslaunch kuka_arm cafe.launch;\
+exec /bin/bash -i'" --tab -e "/bin/bash -c \
+
+'sleep 3;\
+roslaunch kuka_arm spawn_target.launch;\
 exec /bin/bash -i'" --tab -e "/bin/bash -c \
 
 'sleep 5;\
